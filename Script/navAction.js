789998e45c5da50359922b47be3d14ce33651623
Edgar Arteaga <arteduro@gmail.com>
2025-03-28 20:04:24 -0500

    first commit

--- Script/navAction.js
+++
@@ -1,9 +0,0 @@
-const checkBtn = document.querySelector("[data-type-check]");
-const menu = document.querySelector("[data-type-menu]");
-
-const showMenu = () =>{
-    menu.classList.toggle("showMenu");
-    checkBtn.classList.toggle("changeBackground");
-}
-
-checkBtn.addEventListener('click', showMenu);
